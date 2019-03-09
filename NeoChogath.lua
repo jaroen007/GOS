fad9a2bd8764d1dc58545ed86aa86428ff2ed712
jaroen007 <jaroeneefting@hotmail.com>
2019-03-09 18:17:14 +0100

    Update NeoChogath.lua

--- NeoChogath.lua
+++ NeoChogath.lua
@@ -346,24 +346,47 @@ local Menu = MenuElement({name = "Neo Chogath", id = "neochogath", type = MENU, 
 --         champInfo.hasRBuff = false;
 --     end
 -- end
+
+
+
+
+
+
+
+
+
 AddLoadCallback(function()
 	print("texttttt");
 end)
 
-LocalSDK.Orbwalker:CanAttackEvent(function()
-    if (LocalSDK.Spells:CheckSpellDelays({q = 0.250, w = 0.750, e = 0.250, r = 0.500}) and champInfo.hasPBuff == false and champInfo.hasRBuff == false) then
-        return true;
-    end
-    return false;
-end)
+-- LocalSDK.Orbwalker:CanAttackEvent(function()
+--     if (LocalSDK.Spells:CheckSpellDelays({q = 0.250, w = 0.750, e = 0.250, r = 0.500}) and champInfo.hasPBuff == false and champInfo.hasRBuff == false) then
+--         return true;
+--     end
+--     return false;
+-- end)
 
-LocalSDK.Orbwalker:CanMoveEvent(function()
-    if (LocalSDK.Spells:CheckSpellDelays({q = 0.150, w = 0.600, e = 0.150, r = 0.500}) and champInfo.hasRBuff == false) then
-        return true;
-    end
-    return false;
-end)
-    
+-- LocalSDK.Orbwalker:CanMoveEvent(function()
+--     if (LocalSDK.Spells:CheckSpellDelays({q = 0.150, w = 0.600, e = 0.150, r = 0.500}) and champInfo.hasRBuff == false) then
+--         return true;
+--     end
+--     return false;
+-- end)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 -- AddLoadCallback(function()
 --     _G.GamsteronMenuSpell.isaaa:Value(true);
 --     -- TICK
